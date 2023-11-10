7bb6aaf3e270c3ab7738c5529781e1a76f1375da
Your Name <you@example.com>
2023-11-10 14:16:10 -0500

    hice unos cambios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,23 +5,24 @@
 @vite('resources/css/principal.css')
 
 <section class="popular container">
-    <h2> juegos populares</h2>
+    <h2> Juegos Populares</h2>
     <div class="popular-content">
 
-        <img src="/images/mario.jpeg" alt=""> <br>
-        <img src="/images/juegoMario.png" alt="">
-        <img src="/images/Detective Pikachu™ Returns para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="/images/Mario Strikers™ Battle League para la consola Nintendo Switch™ – Sitio oficial.jpeg" alt="">
-        <img src="/images/Super Mario RPG™ para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="/images/EA SPORTS FC™ 24 para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="images/Pikmin 4 para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="images/Pokémon™ Scarlet para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="images/Bayonetta™ 3 – Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="images/Kirby’s Dream Buffet™ para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="images/Xenoblade Chronicles™ 3 para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="images/SONIC SUPERSTARS para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-        <img src="images/Pokémon™ Violet para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt="">
-
+        <ul>
+            <li><img src="/images/mario.jpeg" alt=""> </li>
+            <li><img src="/images/juegoMario.png" alt=""> </li>
+            <li><img src="/images/Detective Pikachu™ Returns para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li><img src="/images/Mario Strikers™ Battle League para la consola Nintendo Switch™ – Sitio oficial.jpeg" alt=""></li>
+            <li> <img src="/images/Super Mario RPG™ para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li><img src="/images/EA SPORTS FC™ 24 para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li> <img src="images/Pikmin 4 para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""> </li>
+            <li> <img src="images/Pokémon™ Scarlet para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li> <img src="images/Bayonetta™ 3 – Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li>  <img src="images/Kirby’s Dream Buffet™ para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li> <img src="images/Xenoblade Chronicles™ 3 para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li> <img src="images/SONIC SUPERSTARS para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+            <li><img src="images/Pokémon™ Violet para Nintendo Switch - Sitio oficial de Nintendo.jpeg" alt=""></li>
+          </ul>
     </div>
     <main class="product-container">
         <h2> Todos los juegos </h2>
@@ -43,17 +44,18 @@
                 </div>
             </div>
             <div class="product-1">
-                <img src="images/juegoMario.png" alt="">
+                <img src="/images/Xenoblade Chronicles™ 3 para Nintendo Switch - Sitio oficial de Nintendo.jpeg"alt="">
 
                 <div class="price">
-                    <h1>Abrefácil Mario Bross</h1>
-                    <p> Año : 1985</p>
-                    <p> Categoria: Aventura</p>
-                    <p> Descripcion: es un videojuego de plataformas, <br>diseñado por Shigeru Miyamoto, <br> lanzado el
-                        13 de septiembre
-                        <br> de 1985 y producido por la empresa <br>
-                        japonesa Nintendo, para la consola Nintendo <br>
-                        Entertainment System.
+                    <h1>Xenoblade Chronicles</h1>
+                    <p> Año : 2009</p>
+                    <p> Categoria: Accion</p>
+                    <p> Descripcion: Xenoblade Chronicles tiene un sistema de acción basado
+                         en la batalla donde los ataques normales pasarán automáticamente
+                          a intervalos, similar al establecido en Final Fantasy XII. Sin embargo, 
+                          los ataques especiales de recarga están por separado
+                         de ese intervalo a través de una "paleta de batalla" en la parte inferior
+                          de la pantalla, parecido al de la saga Dragon Age.
                     </p>
                 </div>
             </div>
@@ -64,10 +66,10 @@
                     <h1>Bayonetta™ 3</h1>
                     <p> Año : 2022</p>
                     <p> Categoria: Accion- Aventura</p>
-                    <p> Descripcion: El juego sigue a la bruja <br>
-                        cazadora de ángeles titular Bayonetta,<br>
-                        que debe enfrentarse a armas biológicas hechas <br>
-                        por el hombre llamadas homúnculos, y una entidad conocida como <br>
+                    <p> Descripcion: El juego sigue a la bruja 
+                        cazadora de ángeles titular Bayonetta,
+                        que debe enfrentarse a armas biológicas hechas 
+                        por el hombre llamadas homúnculos, y una entidad conocida como 
                         la Singularidad que amenaza con destruir el multiverso.
                     </p>
                 </div>
@@ -79,10 +81,10 @@
                     <h1>Kirby’s Dream Buffet</h1>
                     <p> Año : 2022</p>
                     <p> Categoria: Aventura</p>
-                    <p> Descripcion: Compite en carreras <br>
+                    <p> Descripcion: Compite en carreras 
                         a través de apetecibles pistas con obstáculos para obtener
-                        <br>fresas que harán que Kirby crezca. Participa en un minijuego <br>
-                        entre las carreras y después enfréntate en la ronda final,<br>
+                        fresas que harán que Kirby crezca. Participa en un minijuego 
+                        entre las carreras y después enfréntate en la ronda final,
                         una contienda de frutas sobre una plataforma flotante.
                     </p>
                 </div>
@@ -93,11 +95,11 @@
                 <div class="price">
                     <h1>Pokémon™ </h1>
                     <p> Año : 2022</p>
-                    <p> Categoria: rol - imaginacion</p>
-                    <p>Explora un mundo completamente abierto a tu propio paso y <br>
+                    <p> Categoria: rol - imaginacion
+                    <p>Explora un mundo completamente abierto a tu propio paso y 
                         recorre a través de la tierra, el agua y el aire
-                        <br> a lomos del Pokémon legendario Koraidon, <br>
-                        que puede cambiar de forma en Pokémon Scarlet, <br> o sobre el Pokémon legendario Miraidon, <br>
+                         a lomos del Pokémon legendario Koraidon,
+                        que puede cambiar de forma en Pokémon Scarlet,  o sobre el Pokémon legendario Miraidon, 
                         que puede cambiar de forma en Pokémon Violet.
                     </p>
                 </div>
@@ -109,11 +111,11 @@
                     <h1>Detective - pikachu</h1>
                     <p> Año : 2016</p>
                     <p> Categoria: Accion- Aventura-fantasia</p>
-                    <p> Descripcion: El Detective Pikachu es un juego de aventura <br>
-                        en el que los jugadores controlan a Tim Goodman <br>
-                        mientras trabaja junto con el detective Pikachu <br>
-                        para resolver varios misterios. Esto se <br>
-                        logra caminando por escenas, buscando pistas potenciales <br>
+                    <p> Descripcion: El Detective Pikachu es un juego de aventura 
+                        en el que los jugadores controlan a Tim Goodman 
+                        mientras trabaja junto con el detective Pikachu 
+                        para resolver varios misterios. Esto se 
+                        logra caminando por escenas, buscando pistas potenciales 
                         y hablando con personas y Pokémon para descubrir nueva información.
                     </p>
                 </div>
@@ -126,11 +128,11 @@
                     <h1>Mario Strikers</h1>
                     <p> Año : 2005-2006</p>
                     <p> Categoria: Aventura</p>
-                    <p> Descripcion: Strikers es un juego de deportes <br>
-                        que incluye varios de los elementos representativos de la serie de Mario. <br>
-                        El juego presenta los aspectos y objetivos básicos de un título basado en el fútbol, <br>
-                        puesto que el juego no presenta <br>
-                        árbitros y los personajes pueden golpear y atacar a sus <br>
+                    <p> Descripcion: Strikers es un juego de deportes 
+                        que incluye varios de los elementos representativos de la serie de Mario. 
+                        El juego presenta los aspectos y objetivos básicos de un título basado en el fútbol, 
+                        puesto que el juego no presenta 
+                        árbitros y los personajes pueden golpear y atacar a sus 
                         contrincantes de una manera legítima con el fin de quitarles el balón.
                     </p>
                 </div>
@@ -142,9 +144,9 @@
                     <h1>EA SPORTS FC™ 24</h1>
                     <p> Año : 2023</p>
                     <p> Categoria: Futbol Diversion</p>
-                    <p> Descripcion: podrás compartir club con tus amistados <br>
-                        a través del Cross-Play en plataformas de la misma generación. <br>
-                        Esto también da lugar a marcadores unificados para que las personas <br>
+                    <p> Descripcion: podrás compartir club con tus amistados 
+                        a través del Cross-Play en plataformas de la misma generación. 
+                        Esto también da lugar a marcadores unificados para que las personas 
                         que juegan en distintas consolas puedan competir entre sí por posiciones globales.
                     </p>
                 </div>
@@ -156,9 +158,9 @@
                     <h1>Pikmin 4</h1>
                     <p> Año : 2023</p>
                     <p> Categoria: Aventura y Estrategia</p>
-                    <p> Descripcion: Pikmin 4 es un juego de aventura y estrategia <br>
-                        en tiempo real para Nintendo Switch que, <br>
-                        continuando con la exitosa saga Pikmin, presenta una propuesta divertida, <br>
+                    <p> Descripcion: Pikmin 4 es un juego de aventura y estrategia 
+                        en tiempo real para Nintendo Switch que, 
+                        continuando con la exitosa saga Pikmin, presenta una propuesta divertida, 
                         sencilla y encantadora con desafíos de todo tipo desarrollada por la Gran N.
                     </p>
                 </div>
